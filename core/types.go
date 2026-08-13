249627b4b0b748f7dee70f8c454a4540260b5353
joabssilveira <joabssilveira@gmail.com>
2026-08-13 00:03:07 -0300

    refactor:Specific GORM types that were in the core have been moved to the correct package.

--- core/types.go
+++ core/types.go
@@ -1,14 +1,5 @@
 package fwork_server_orm
 
-import (
-	"database/sql/driver"
-	"encoding/json"
-	"fmt"
-
-	"gorm.io/gorm"
-	"gorm.io/gorm/schema"
-)
-
 // request...
 
 type QueryPayload struct {
@@ -96,62 +87,4 @@ type QueryBuilder interface {
 type Order struct {
 	Field string `json:"field"`
 	Dir   string `json:"dir"` // asc | desc
-}
-
-// DB TYPES
-
-// JSONB is a generic wrapper for saving any struct/slice/map as jsonb in Postgres.
-type JSONB[T any] struct {
-	Data T
-}
-
-//
-// =======================
-// DATABASE (GORM / SQL)
-// =======================
-//
-
-// Value converts to JSON before saving to the database
-func (j JSONB[T]) Value() (driver.Value, error) {
-	return json.Marshal(j.Data)
-}
-
-func (JSONB[T]) GormDataType() string {
-	return "jsonb"
-}
-
-func (JSONB[T]) GormDBDataType(db *gorm.DB, field *schema.Field) string {
-	return "jsonb"
-}
-
-// Scan converts JSON from the database back to the Go type.
-func (j *JSONB[T]) Scan(value interface{}) error {
-	if value == nil {
-		var empty T
-		j.Data = empty
-		return nil
-	}
-
-	bytes, ok := value.([]byte)
-	if !ok {
-		return fmt.Errorf("dbtypes.JSONB: invalid type Scan")
-	}
-
-	return json.Unmarshal(bytes, &j.Data)
-}
-
-//
-// =======================
-// API (JSON HTTP)
-// =======================
-//
-
-// MarshalJSON makes the API return only the content, without "Data".
-func (j JSONB[T]) MarshalJSON() ([]byte, error) {
-	return json.Marshal(j.Data)
-}
-
-// UnmarshalJSON allows you to receive JSON directly in the field.
-func (j *JSONB[T]) UnmarshalJSON(b []byte) error {
-	return json.Unmarshal(b, &j.Data)
 }
